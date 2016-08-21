6ac684d09b512ffdc389ed6f31ecea52f570648c
Yashraj Arora <yashrajarora@gmail.com>
2016-08-22 00:11:07 +0800

    New Feature added

---
+++ com/bread/demo/BreadFeature.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bread.demo;
+/**
+ * @author Yash
+ *
+ */
+public class BreadFeature {
+	public static void main(String[] args) {
+		System.out.println("New Feature is here");
+		System.out.println("First cut");
+	}
+
+}
