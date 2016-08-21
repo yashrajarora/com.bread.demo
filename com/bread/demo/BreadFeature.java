26a438f07277051179b7ea69d76e422c2a003552
Yashraj Arora <yashrajarora@gmail.com>
2016-08-22 00:36:42 +0800

    Finished coding for new feature

--- com/bread/demo/BreadFeature.java
+++ com/bread/demo/BreadFeature.java
@@ -9,7 +9,7 @@ package com.bread.demo;
 public class BreadFeature {
 	public static void main(String[] args) {
 		System.out.println("New Feature is here");
-		System.out.println("First cut");
+		System.out.println("Finished feature");
 	}
 
 }
